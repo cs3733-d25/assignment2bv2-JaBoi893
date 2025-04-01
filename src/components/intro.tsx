c23e7ee65a44023b0c8c157f6de997c2a7b2115b
thomas <git@thomasricci.dev>
2025-04-01 17:15:46 -0400

    add my intro

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -3,7 +3,13 @@ import { FC, ReactElement } from "react";
 export const Intro: FC = (): ReactElement => {
   return (
     <>
-      <h2>Our Hobbies:</h2>
+      <h2>Group Members:</h2>
+      <p>
+
+      </p>
+      <p>
+        My name's Thomas Ricci. I'm a sophomore CS major.
+      </p>
     </>
   );
 };
