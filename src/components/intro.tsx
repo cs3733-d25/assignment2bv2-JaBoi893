90705f4a6c5cebc839452b7e902c7c213bf0ed88
JaBoi893 <perveilerjr@gmail.com>
2025-04-01 17:19:36 -0400

    App, title, and hobby1 complete(?)

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -5,7 +5,7 @@ export const Intro: FC = (): ReactElement => {
     <>
       <h2>Group Members:</h2>
       <p>
-
+        My name's John Perveiler. I'm a sophomore CS major.
       </p>
       <p>
         My name's Thomas Ricci. I'm a sophomore CS major.
